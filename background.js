4d3b017148cf9326daa976ae29735cdbf421da80
Miguel Rosa <miguel-pedro@live.com>
2025-02-06 01:55:20 +0000

    Initial commit

---
+++ background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: [
+        {
+          id: 1,
+          priority: 1,
+          action: {
+            type: "redirect",
+            redirect: {
+              regexSubstitution: "https://wiki.warframe.com/w/\\1"
+            }
+          },
+          condition: {
+            regexFilter: "^https://warframe\\.fandom\\.com/wiki/(.+)$",
+            resourceTypes: ["main_frame"]
+          }
+        }
+      ],
+      removeRuleIds: [1]
+    });
+  });
+  
